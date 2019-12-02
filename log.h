1c2fd01d47ba3f470abd1c6a186e4d95d14b7e81
root <hankai17@126.com>
2019-12-01 19:01:51 -0800

    add appender formatter

--- log.h
+++ log.h
@@ -6,18 +6,19 @@
 #define MY_SYLAR_LOG_H
 
 #include<memory>
+#include<list>
 
 namespace slylar {
 
     class LogLevel {
     public:
         enum Level {
-            UNKNOW = 0,
-            DEBUG = 1,
-            INFO = 2,
-            WARN = 3,
-            ERROR = 4,
-            FATAL = 5
+            UNKNOW  = 0,
+            DEBUG   = 1,
+            INFO    = 2,
+            WARN    = 3,
+            ERROR   = 4,
+            FATAL   = 5
         };
     };
 
@@ -25,14 +26,27 @@ namespace slylar {
 
     };
 
+    class LogFormatter {
+    public:
+        typedef std::shared_ptr<LogFormatter> ptr;
+    private:
+
+    };
+
+    class LogAppender { // Where to log
+    public:
+        typedef std::shared_ptr<LogAppender> ptr;
+        virtual ~LogAppender() {}; // This class Must be publiced, so use virtual deconstruct
+    };
+
     class Logger {
     public:
         typedef std::shared_ptr<Logger> ptr;
     private:
-        std::string m_name;
-        LogLevel::Level m_level;
-        //output where
-        //formatter //
+        std::string                 m_name;
+        LogLevel::Level             m_level;
+        std::list<LogAppender::ptr> m_appenders; // Use ptr
+        LogFormatter::ptr           m_formatter;
     };
 
 }
