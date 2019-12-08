bda84f7c321b415a5643d3807dd3a577c5359ebd
root <hankai17@126.com>
2019-12-07 18:23:09 -0800

    config test done

--- config.cc
+++ config.cc
@@ -5,13 +5,13 @@ namespace sylar {
 
     Config::ConfigVarMap Config::m_config; //!!!
 
-    static ConfigVarBase::ptr LookupBase(const std::string& name) { // It just a simple judge
+    ConfigVarBase::ptr Config::LookupBase(const std::string& name) { // It just a simple judge
         auto it = Config::getMap().find(name);
         return it == Config::getMap().end() ? nullptr : it->second; // Not use m_data[name]
     }
 
     static void ListAllMember(const std::string& prefix, const YAML::Node& node,
-            std::list<std::pair<std::string, const YAML::Node> > all_nodes) {
+            std::list<std::pair<std::string, const YAML::Node> >& all_nodes) {
         if (prefix.find_first_not_of("abcdefghijklmnopqrstuvwxyz._0123456789") != std::string::npos) {
             SYLAR_LOG_ERROR(SYLAR_LOG_ROOT()) << "Config invalid name: " <<  prefix;
             return;
@@ -19,22 +19,21 @@ namespace sylar {
         all_nodes.push_back(std::make_pair(prefix, node));
         if (node.IsMap()) {
             for (auto& it : node) { // const auto& it
-                    /*
                     ListAllMember(prefix.empty() ? it.first.Scalar() : prefix + "." + it.first.Scalar(), it.second, all_nodes);
-                    */
             }
         }
     }
 
-    static void loadFromYaml(const YAML::Node& root) {
+    void Config::loadFromYaml(const YAML::Node& root) {
         std::list<std::pair<std::string, const YAML::Node> > all_nodes;
         ListAllMember("", root, all_nodes);
-/*
-        //for (auto& i : all_nodes) // for (auto i : all_nodes) {
+
+        for (auto& i : all_nodes) { // for (auto i : all_nodes)
             std::string key = i.first;
             if (key.empty()) {
                 continue;
             }
+            SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << "key: " << key;
 
             std::transform(key.begin(), key.end(), key.begin(), ::tolower);
             ConfigVarBase::ptr var = LookupBase(key);
@@ -48,7 +47,6 @@ namespace sylar {
                 }
             }
         }
-*/
     }
 
 }
--- tests/config_test.cc
+++ tests/config_test.cc
@@ -9,35 +9,41 @@ sylar::ConfigVar<float>::ptr g_float_value_config =
 
 void print_yaml(const YAML::Node& node) { // yaml 0.6.3
     if (node.IsScalar()) {
-        //SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << node.Scalar();
+        SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << "scalar--->" << node.Scalar();
     } else if (node.IsMap()) {
-        /*
-        for (const auto&i : node) { //TODO
-            //SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << i.first << " " << i.second;
+        for (const auto&it : node) {
+            SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << "maps--->" << it.first; // << " " << it.second << "--->in maps";
+            print_yaml(it.second);
         }
-         */
     } else if (node.IsSequence()) {
-        /*
-        for (const auto &i : node) {
-            //SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << i.first << " " << i.second;
+        for (const auto &it : node) {
+            SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << "seqs--->" << it.first;
+            print_yaml(it);
         }
-         */
+    } else if (node.IsNull()) {
+        SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << "null--->";
     }
 }
 
 void test_yaml() {
     YAML::Node root = YAML::LoadFile("/root/CLionProjects/my_sylar/tests/log.yml");
     SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << root;
+    SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << "-------------------------";
     print_yaml(root);
 }
 
-
+void test_config() {
+    YAML::Node root = YAML::LoadFile("/root/CLionProjects/my_sylar/tests/log.yml");
+    sylar::Config::loadFromYaml(root);
+    SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << "after load";
+    SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << g_int_value_config->getValue();
+    SYLAR_LOG_DEBUG(SYLAR_LOG_ROOT()) << g_float_value_config->getValue();
+}
 
 int main(int argc, char** argv) {
     sylar::Logger::ptr logger = sylar::Logger::getLoggerInstance();
     logger->addAppender(sylar::LogAppender::ptr(new sylar::StdoutLogAppender));
-
-    test_yaml();
-
+    //test_yaml();
+    test_config();
     return 0;
 }
