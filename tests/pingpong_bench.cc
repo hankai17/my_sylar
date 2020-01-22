000218699f3b8b4342947a562f1b92d3243ccda3
root <hankai17@126.com>
2020-01-22 06:08:01 -0800

    10 Times slow libevent2

--- tests/pingpong_bench.cc
+++ tests/pingpong_bench.cc
@@ -1,5 +1,6 @@
 #include "iomanager.hh"
 #include "log.hh"
+#include "util.hh"
 
 #include <sys/resource.h>
 #include <sys/socket.h>
@@ -13,38 +14,46 @@ int numActive;
 int numWrites;
 int g_reads, g_writes, g_fired;
 sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
+uint64_t flower_start = 0;
+uint64_t flower_end = 0;
 
-void fd_test(int fd) {
-    //SYLAR_LOG_DEBUG(g_logger) << "fd: " << fd;
-    sylar::IOManager::GetThis()->addEvent(fd, sylar::IOManager::READ, [=]() {
+void fd_test(int idx) {
+    sylar::IOManager::GetThis()->addEvent(g_pipes[idx * 2], sylar::IOManager::READ, [=]() {
         char ch;
-        g_reads += static_cast<int>(::recv(fd, &ch, sizeof(ch), 0));
+        g_reads += static_cast<int>(::recv(g_pipes[idx * 2], &ch, sizeof(ch), 0));
         if (g_writes > 0) {
-          int widx = fd + 1;
+          int widx = idx + 1;
           if (widx >= numPipes) {
-            widx -= numPipes;
+            //widx -= numPipes;
+            //close
+            flower_end = sylar::GetCurrentUs();
+            SYLAR_LOG_DEBUG(g_logger) << "flower_end: " << flower_end;
+            SYLAR_LOG_DEBUG(g_logger) << "elapse: " << flower_end - flower_start;
+          } else {
+              if (idx == 0) {
+                  flower_start = sylar::GetCurrentUs();
+                  SYLAR_LOG_DEBUG(g_logger) << "flower_sta: " << flower_start;
+              }
+              ::send(g_pipes[2 * widx + 1], "m", 1, 0);
+              //SYLAR_LOG_DEBUG(g_logger) << "fd: " << g_pipes[idx * 2] << " read callback " << ", send fd: "
+                                        //<< g_pipes[2 * widx + 1];
+              g_writes--;
           }
-          ::send(g_pipes[2 * widx + 1], "m", 1, 0);
-          SYLAR_LOG_DEBUG(g_logger) << "fd: "<< fd << " read callback " << ", send fd: " << g_pipes[2 * widx + 1];
-          g_writes--;
           g_fired++;
+          sylar::IOManager::GetThis()->delEvent(g_pipes[idx * 2], sylar::IOManager::READ);
+          close(g_pipes[idx * 2]);
+          close(g_pipes[idx * 2 + 1]);
+          //SYLAR_LOG_DEBUG(g_logger) << "delEvent fd: " << g_pipes[idx * 2] << "  close it and " << g_pipes[idx * 2 + 1];
         }
-        if (g_fired == g_reads) {
-            SYLAR_LOG_DEBUG(g_logger) << "g_fired == g_reads";
-        }
-
     });
 
 }
 
 int main(int argc, char* argv[]) {
-    numPipes = 100;
+    numPipes = 10000;
     numActive = 1;
-    numWrites = 100;
-
-    g_fired = numActive;
-    g_reads = 0;
-    g_writes = numWrites;
+    numWrites = 10000; // result 150ms however libevent2 15ms. 10 Times slow
+    g_fired = 0; // Sum send bytes
 
     int c;
     while ((c = getopt(argc, argv, "n:a:w:")) != -1) {
@@ -63,6 +72,7 @@ int main(int argc, char* argv[]) {
           return 1;
       }
     }
+    g_writes = numWrites; // Sum Write count
 
     struct rlimit rl;
     rl.rlim_cur = rl.rlim_max = numPipes * 2 + 50;
@@ -82,20 +92,22 @@ int main(int argc, char* argv[]) {
     }
 
     for (int i = 0; i < numPipes * 2; ++i) {
-        SYLAR_LOG_DEBUG(g_logger) << "g_pipes[" << i << "]: " << g_pipes[i];
+        //SYLAR_LOG_DEBUG(g_logger) << "g_pipes[" << i << "]: " << g_pipes[i];
     }
 
     int space = numPipes / numActive;
     space *= 2;
     for (int i = 0; i < numActive; i++, g_fired++) {
         int ret = send(g_pipes[i * space + 1], "e", 1, 0);
-        SYLAR_LOG_DEBUG(g_logger) << "send fd: " << g_pipes[i * space + 1] << ", send ret: " << ret;
+        g_fired++;
+        SYLAR_LOG_DEBUG(g_logger) << "init send fd: " << g_pipes[i * space + 1] << ", ret: " << ret;
     }
       
     sylar::IOManager io(1, false, "iomanager");
-
+    //sleep(2);
+    SYLAR_LOG_DEBUG(g_logger) << "schedu: " << sylar::GetCurrentUs();
     for (int i = 0; i < numPipes; ++i) {
-      io.schedule(std::bind(fd_test, g_pipes[i*2]));
+      io.schedule(std::bind(fd_test, i));
     }
     io.stop();
     return 0;
