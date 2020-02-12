b22e725890dc7a9be85e17d5263a1de88e73626a
root <hankai17@126.com>
2020-02-11 18:47:28 -0800

    add http req & resp

--- http/http.cc
+++ http/http.cc
@@ -14,7 +14,8 @@ namespace sylar {
         HttpRequest::HttpRequest(uint8_t version, bool close)
         : m_method(HttpMethod::GET),
         m_version(version),
-        m_close(close) {
+        m_close(close),
+        m_path("/") {
         }
 
         std::string HttpRequest::getPara(const std::string& key, const std::string& def) const {
@@ -112,7 +113,19 @@ namespace sylar {
             return s_method_string[idx];
         }
 
-        std::ostream& HttpRequest::dump(std::ostream &os) {
+        const char* HttpStatusToString(const HttpStatus& m) {
+            switch (m) {
+#define XX(num, name, string) \
+    case HttpStatus::name :  \
+        return #string;
+        HTTP_STATUS_MAP(XX);
+#undef XX
+                default:
+                    return "<unknown>";
+            }
+        }
+
+        std::ostream& HttpRequest::dump(std::ostream &os) const {
             // GET / HTTP/1.1
             // Host: www.ifeng.com
 
@@ -143,6 +156,67 @@ namespace sylar {
                 os << "\r\n";
             }
             return os;
+        }
+
+        std::string HttpRequest::toString() const {
+            std::stringstream ss;
+            dump(ss);
+            return ss.str();
+        }
+
+        HttpResponse::HttpResponse(uint8_t version, bool close)
+        : m_status(HttpStatus::OK),
+        m_version(version),
+        m_close(close) {
+        }
+
+        std::string HttpResponse::getHeader(const std::string& key, const std::string& def) const {
+            auto it = m_headers.find(key);
+            if (it == m_headers.end()) {
+                return def;
+            }
+            return it->second;
+        }
+
+        void HttpResponse::setHeader(const std::string& key, const std::string& val) {
+            m_headers[key] = val;
+        }
+
+        void HttpResponse::delHeader(const std::string& key) {
+            m_headers.erase(key);
+        }
+
+        std::ostream& HttpResponse::dump(std::ostream& os/*in-return*/) const {
+            // HTTP/1.1 200 OK
+            os << "HTTP/"
+            << (((uint32_t)m_version) >> 4)
+            << "."
+            << ((uint32_t)m_version & 0x0F)
+            << " "
+            << (uint32_t)m_status
+            << " "
+            << ((m_reason.empty()) ? HttpStatusToString(m_status) : m_reason) << "\r\n";
+
+            for (const auto& i : m_headers) {
+                if (strcasecmp(i.first.c_str(), "connection") == 0) {
+                    continue;
+                }
+                os << i.first << ": " << i.second << "\r\n";
+            }
+            os << "Connection: " << ((m_close) ? "close" : "keep-alive") << "\r\n";
+            if (!m_body.empty()) {
+                os << "Content-Length: " << m_body.size() << "\r\n\r\n";
+                os << m_body;
+            } else {
+                os << "\r\n";
+            }
+            return os;
+        }
+
+        std::string HttpResponse::toString() const {
+            std::stringstream ss;
+            dump(ss);
+            return ss.str();
         }
     }
 }
--- http/http.hh
+++ http/http.hh
@@ -149,6 +149,8 @@ namespace sylar {
       const MapType& getParas() const { return m_paras; }
       const MapType& getHeaders() const { return m_headers; }
       const MapType& getCookies() const { return m_cookies; }
+      bool isClose() const { return m_close; }
+      void setClose(bool v) { m_close = v; }
 
       void setMethod(HttpMethod v) { m_method = v; }
       void setStatus(HttpStatus v) { m_status = v; }
@@ -189,7 +191,8 @@ namespace sylar {
         return checkGetAs(m_cookies, key, val, def);
       }
 
-      std::ostream& dump(std::ostream& os/*in-return*/);
+      std::ostream& dump(std::ostream& os/*in-return*/) const;
+      std::string toString() const;
 
   private:
       template <typename T>
@@ -234,6 +237,64 @@ namespace sylar {
       MapType         m_paras;
       MapType         m_headers;
       MapType         m_cookies;
+  };
+
+  class HttpResponse {
+  public:
+      typedef std::shared_ptr<HttpResponse> ptr;
+      typedef std::map<std::string, std::string, CaseInsensitiveLess> MapType;
+
+      HttpResponse(uint8_t version = 0x11, bool close = true);
+      HttpStatus getStatus() const { return m_status; }
+      uint8_t getVersion() const { return m_version; }
+      const MapType& getHeaders() const { return m_headers; }
+      const std::string& getBody() const { return m_body; }
+      const std::string& getReason() const { return m_reason; }
+
+      void setStatus(HttpStatus v) { m_status = v; }
+      void setVersion(uint8_t v) { m_version = v; }
+      void setHeaders(const MapType& v) { m_headers = v; }
+      void setBody(const std::string& v) { m_body = v; }
+      void setReason(const std::string& v) { m_reason = v; }
+
+      bool isClose() const { return m_close; }
+      void setClose(bool v) { m_close = v; }
+
+      std::string getHeader(const std::string& key, const std::string& def = "") const;
+      void setHeader(const std::string& key, const std::string& val);
+      void delHeader(const std::string& key);
+
+      template <typename T>
+      bool checkHeaderAs(const std::string& key, T& val/*in-out*/, const T& def = T()) {
+        return checkGetAs(m_headers, key, val, def);
+      }
+
+      std::ostream& dump(std::ostream& os/*in-return*/) const;
+      std::string toString() const;
+
+  private:
+      template <typename T>
+      bool checkGetAs(const MapType& m, const std::string& key, T& val/*in-out*/, const T& def = T()) {
+        auto it = m.find(key);
+        if (it == m.end()) {
+          val = def;
+          return false;
+        }
+        try {
+          val = boost::lexical_cast<T>(it->second);
+        } catch (...) {
+          val = def;
+        }
+        return false;
+      }
+
+  private:
+      HttpStatus    m_status;
+      uint8_t       m_version;
+      std::string   m_reason;
+      MapType       m_headers;
+      bool          m_close;
+      std::string   m_body;
   };
 
 
