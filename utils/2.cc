fabed7d8c8a2d1f18aa33e17b07c2df6609f0a92
root <hankai17@126.com>
2020-03-31 09:22:45 +0800

    add std::bind test

--- utils/2.cc
+++ utils/2.cc
@@ -26,12 +26,29 @@ int main1() {
     return 0;
 }
 
+void test(int a, int b) {
+  a = 1;
+  std::cout << "a: " << a <<  ", b: " << b << std::endl;
+}
+
 int main() {
     std::shared_ptr<Foo> pFoo(new Foo);
     std::cout << "1pFoo.use_count(): " << pFoo.use_count() << std::endl;
     std::function<void()> f = std::bind(&Foo::doit, pFoo); // bind函数延长了对象的生命周期
     std::cout << "2pFoo.use_count(): " << pFoo.use_count() << std::endl;
+
+    std::function<void(void)> f1 = std::bind(test, 1, 2);
+    f1();
+
+    int a = 1;
+    int b = 99;
+    std::function<void()> f2 = [a, b](int x, int y){
+      std::cout<<"a: " << a << ", b:" << b << std::endl;
+    };
+    f2(1,1);
     return 0;
 }
 
 // https://blog.csdn.net/tsing_best/article/details/25055509
+
+// https://www.jianshu.com/p/d22904f30930 bind tuple实现原理 可变模板参数 偏特化
